73ecbea45a5bbd73509ec467eb5a73f8f660f02f
Brian Michel <brian.michel@gmail.com>
2015-04-19 10:27:29 -0400

    removing testing target from example

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  ConsoleBannerTests.m
-//  ConsoleBannerTests
-//
-//  Created by Brian Michel on 04/19/2015.
-//  Copyright (c) 2014 Brian Michel. All rights reserved.
-//
-
-${TEST_EXAMPLE}
